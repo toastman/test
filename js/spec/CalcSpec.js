89134ab1e10807bdd166d8d1ea8d3f6dd39e9fb8
GitToast <gitva@ukr.net>
2013-07-27 09:11:55 +0300

    adding tests

--- js/spec/CalcSpec.js
+++ js/spec/CalcSpec.js
@@ -5,3 +5,18 @@
  * Time: 10:24 AM
  * To change this template use File | Settings | File Templates.
  */
+describe('Calculator',function(){
+  var calc;
+
+  beforeEach(function(){
+     calc = new Calc();
+  });
+
+  it("should be able to add two numbers", function(){
+     expect(calc.addNumbers(2,3)).toEqual(5);
+  });
+
+  it("should be able to subtract two numbers", function(){
+     expect(calc.substrNumbers(4,3)).toEqual(1);
+  });
+});
